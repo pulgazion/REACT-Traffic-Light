063cd26dd2686d0dce7b7a2eb61ada1d18ceff37
pulgazion <reimarrodri@gmail.com>
2023-01-26 13:25:43 +0000

    Proyeto Semaforo en React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+export const Home = () => {
+	const [color, setColor] = useState("red");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container my-5">
+			<div className="container bg-dark p-3">
+				<div
+					className={
+						"light red " + (color == "red" ? "selected" : "")
+					}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						"light yellow " + (color == "yellow" ? "selected" : "")
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						"light green " + (color == "green" ? "selected" : "")
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
+		</div>		
 	);
 };
-
-export default Home;
